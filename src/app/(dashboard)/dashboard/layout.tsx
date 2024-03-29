6026fb336e7890319a73cd03d61fc41dd8819dd4
DavidCebzan <david.cebzan@alianza.com>
2024-03-29 11:56:06 +0100

    add more styles to the containers

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -134,7 +134,11 @@ const Layout = async ({ children }: LayoutProps) => {
                     </ul>
                 </nav>
             </div>
+            <aside
+            className=' max-h-screen container py-16 md:py-12 w-full'
+            >
             {children}
+            </aside>
         </div>
     )
 }
