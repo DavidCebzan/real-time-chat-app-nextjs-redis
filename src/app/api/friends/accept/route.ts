d8d73e3ff036a7bfcd7c524cd8a0f80d9a529e3b
DavidCebzan <david.cebzan@alianza.com>
2024-04-02 13:22:13 +0200

    refactore accept route for optimizing performance

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -9,58 +9,75 @@ import { z } from "zod";
 export async function POST(req: Request) {
     try {
         const body = await req.json();
-        
-        const {id: idToAdd} = z.object({id: z.string()}).parse(body);
+
+        const { id: idToAdd } = z.object({ id: z.string() }).parse(body);
 
         const session = await getServerSession(authoOptions);
 
-        if(!session) {
-            return new Response('Unauthorised', {status: 401})
+        if (!session) {
+            return new Response('Unauthorised', { status: 401 })
         }
 
         //verify both user are not already friends
 
-        const isAlreadyFried = await fetchRedis('sismember', 
-        `user:${session.user.id}:friends`,
-        idToAdd
+        const isAlreadyFried = await fetchRedis('sismember',
+            `user:${session.user.id}:friends`,
+            idToAdd
         )
 
-        if(isAlreadyFried) {
-            return new Response('Already Firends', {status: 400});
+        if (isAlreadyFried) {
+            return new Response('Already Firends', { status: 400 });
         }
 
-        const hasFriendRequest = await fetchRedis('sismember', 
-        `user:${session.user.id}:incoming_friend_request`,
-        idToAdd,
+        const hasFriendRequest = await fetchRedis('sismember',
+            `user:${session.user.id}:incoming_friend_request`,
+            idToAdd,
         )
 
-        if(!hasFriendRequest) {
-            return new Response('No friend request', {status: 400})
+        if (!hasFriendRequest) {
+            return new Response('No friend request', { status: 400 })
         }
+
+
+        const [userRaw, friendRaw] = (await Promise.all(
+            [
+                fetchRedis('get', `user:${session.user.id}`),
+                fetchRedis('get', `user:${idToAdd}`)
+            ]
+        )) as [string, string];
+
+        const user = JSON.parse(userRaw) as User;
+        const friend = JSON.parse(friendRaw) as User;
 
         //notify added user
 
-        pusherServer.trigger(
-         toPuherKey(`user:${idToAdd}:friends`),
-         'new_friend',
-         ''
-         )
 
-        await db.sadd(`user:${session.user.id}:friends`, idToAdd);
+        await Promise.all([
+            pusherServer.trigger(
+                toPuherKey(`user:${idToAdd}:friends`),
+                'new_friend',
+                user
+            ),
+            pusherServer.trigger(
+                toPuherKey(`user:${session.user.id}:friends`),
+                'new_friend',
+                friend
+            ),
+            db.sadd(`user:${session.user.id}:friends`, idToAdd),
 
-        await db.sadd(`user:${idToAdd}:friends`, session.user.id);
+            db.sadd(`user:${idToAdd}:friends`, session.user.id),
 
-        await db.srem(`user:${session.user.id}:incoming_friend_request`, idToAdd);
-
+            db.srem(`user:${session.user.id}:incoming_friend_request`, idToAdd)
+        ]);
 
         return new Response('OK');
-        
+
     } catch (error) {
-        
+
         if (error instanceof z.ZodError) {
-            return new Response('Invali request payload', {status: 422});
+            return new Response('Invali request payload', { status: 422 });
         }
 
-        return new Response('Ivalid request', {status: 400})
+        return new Response('Ivalid request', { status: 400 })
     }
 }
